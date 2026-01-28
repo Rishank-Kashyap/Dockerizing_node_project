0a72d51f5089750ac8d023da8f8daea56e70e2dc
Rishank Kashyap <rishankkashyap03@example.com>
2026-01-28 21:01:51 +0530

    Feat: Added dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
 app.get("/home", (req, res, next) => {
   res.status(200).json({ message: "You are at Home Page" });
 });
 
-app.listen(3000, () => {
-  console.log("App started at port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
 });
